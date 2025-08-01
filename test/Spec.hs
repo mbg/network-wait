4d6f7bedce4da4163e7242242a6e6c116a3ece25
Michael B. Gale <github@michael-gale.co.uk>
2025-08-01 23:26:50 +0100

    Set timeout for test case

--- test/Spec.hs
+++ test/Spec.hs
@@ -57,7 +57,7 @@ tests = testGroup "network-wait"
         case res of
             Left _ -> pure ()
             Right _ -> assertFailure "`waitTcp` did not fail"
-    , testCase "Can't connect to non-routable private IP" $ do
+    , localOption (mkTimeout $ 10*1000*1000) $ testCase "Can't connect to non-routable private IP" $ do
         res <- try @IO @SomeException $
             waitTcp testRetryPolicy "10.255.255.1" "80"
 
